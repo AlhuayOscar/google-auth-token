55a121f3538e2080a3aa24569774aa0ab33a6151
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-09 20:56:52 -0300

    Añadiendo la estructura del Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { gapi } from "gapi-script";
+import GoogleLogin from "react-google-login";
+import { useEffect } from "react";
 
 function App() {
+  const clientID =
+    "155420643556-kumc4k1g46jq1bmoqqjavm5h9ltas5ok.apps.googleusercontent.com";
+  const onSuccess = (response) => {
+    setUser(response.profileObj);
+    document.getElementsByClassName("btn").hidden = true;
+  };
+  const onFailure = (response) => {
+    console.log("Something went wrong");
+  };
+  const handleLogout = () => {
+    setUser({});
+  };
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientID,
+      });
+    }
+    gapi.load("client:auth2", start);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <h1>Login</h1>
+
+      <div className="btn">
+        <GoogleLogin
+          clientId={clientID}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          buttonText="Continue  with Google"
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+
+      <div class={user ? "profile" : "hidden"}>
+        <img src={user.imageUrl} />
+        <h3>{user.name}</h3>
+      </div>
     </div>
   );
 }
