4c4e8187d9e8fc43619998eebc8a1576925b3e64
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-16 22:55:26 -0300

    Login Andando

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ function App() {
     "155420643556-kumc4k1g46jq1bmoqqjavm5h9ltas5ok.apps.googleusercontent.com";
   const [user, setUser] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
-
   const onSuccess = (response) => {
+    const token = generateToken(64);
     setUser({
       ...response.profileObj,
       userId: response.googleId,
       email: response.profileObj.email,
+      token: token,
     });
     setLoggedIn(true);
+    localStorage.setItem("token", token);
   };
   const onFailure = (response) => {
     console.log("Something went wrong");
@@ -23,7 +25,15 @@ function App() {
   const handleLogout = () => {
     setUser({});
     setLoggedIn(false);
+    localStorage.removeItem("token");
   };
+  function generateToken(length) {
+    const buffer = new Uint8Array(length / 2); //La matematica para el token
+    crypto.getRandomValues(buffer);
+    return Array.prototype.map
+      .call(buffer, (x) => ("0" + x.toString(16)).slice(-2))
+      .join("");
+  }
   useEffect(() => {
     function start() {
       gapi.client.init({
@@ -31,12 +41,15 @@ function App() {
       });
     }
     gapi.load("client:auth2", start);
+    //Esto es solo para eliminar el token al refrescar:
+    window.addEventListener("beforeunload", () => {
+      localStorage.removeItem("token");
+    });
   }, []);
   console.log(loggedIn, handleLogout);
   return (
     <div className="center">
       <h1>Login</h1>
-
       <div className="btn">
         <GoogleLogin
           clientId={clientID}
@@ -46,7 +59,6 @@ function App() {
           cookiePolicy={"single_host_origin"}
         />
       </div>
-
       {loggedIn && (
         <div className="profile">
           <img src={user.imageUrl} alt="User Profile" />
